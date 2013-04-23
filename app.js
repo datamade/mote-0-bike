595b414e5c92552e920691f08e8f265b0739db85
Nick Doiron <ndoiron@mapmeld.com>
2013-04-23 12:35:10 -0500

    remove example model

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config');
-var example_model = require('./models/example');
 var trip_model = require('./models/trip');
 
 var db_uri = process.env.MONGOLAB_URI || process.env.MONGODB_URI || "mongodb://localhost/mote0bike";
@@ -11,7 +10,6 @@ var app = module.exports = express.createServer();
 config(app, express);
 
 var models = {};
-models.examples = example_model(mongoose);
 models.trips = trip_model(mongoose);
 
 require('./routes')(app, models);
